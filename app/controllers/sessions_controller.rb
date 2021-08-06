18e9501907b6612276c338419c3a626276b0021c
makidai <gachapinninaritaina@yahoo.co.jp>
2021-08-06 07:34:01 +0000

    remember

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,8 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate( params[:session][:password])
       log_in(user)
+      # 三項演算子。1ならrememberで、1がなければforgetにする。
+      if params[:session][:remember_me] == '1'? remember(user) : forget(user)
       redirect_to user
     else
       flash[:danger] = "メールアドレスかパスワードが間違っています"
@@ -14,7 +16,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy 
-    log_out
+    log_out if logged_in?
     redirect_to root_path
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   # 強制的に小文字切り替え
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
@@ -11,4 +12,29 @@ class User < ApplicationRecord
   validates :password, presence: true, length: { minimum: 6 }
   # パスワードのいろんな機能が使えるようになる
   has_secure_password
+
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember 
+    self.remember_token = User.new_token 
+    update_attribute(:remember_digest, User.digest(self.remember_token))
+  end
+
+  # 渡されたトークンがremember_digestと一致したらtrueを返す
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).id_password?(remember_token)
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
 end
