949a7cb0adac02ea2dd3bb79400647352301db43
makidai <gachapinninaritaina@yahoo.co.jp>
2021-08-12 07:34:24 +0000

    yokaro

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,13 +2,14 @@ class Project < ApplicationRecord
 
   belongs_to :user
 
-  validates :goal, presence: true
-  validates :step_1, presence: true
-  validates :step_2, presence: true
-  validates :step_3, presence: true
-  validates :step_4, presence: true
-  validates :step_5, presence: true
-  validates :step_6, presence: true
+  validates :goal, presence: true, length: { maximum: 30 }
+  validates :step_1, presence: true, length: { maximum: 30 }
+  validates :step_2, presence: true, length: { maximum: 30 }
+  validates :step_3, presence: true, length: { maximum: 30 }
+  validates :step_4, length: { maximum: 30 }
+  validates :step_5, length: { maximum: 30 }
+  validates :step_6, length: { maximum: 30 }
+
   validates :user_id, presence: true
 
 end
